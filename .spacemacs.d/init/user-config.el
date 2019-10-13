59372c74b3fd86a4602927f4d5f4fa2727c128b2
Rohit Ramesh <rohit507@gmail.com>
2019-10-13 14:01:09 -0700

    Added Hexchat config along

--- .spacemacs.d/init/user-config.el
+++ .spacemacs.d/init/user-config.el
@@ -15,7 +15,7 @@
   (dotspacemacs/user-config/fish-color)
   (dotspacemacs/user-config/undo-tree-settings)
   (dotspacemacs/user-config/haskell-indent-settings)
-  (dotspacemacs/user-config/haskell-hare)
+  ;;(dotspacemacs/user-config/haskell-hare)
 
   (with-eval-after-load 'intero
     (flycheck-add-next-checker 'intero '(warning . haskell-hlint)))
